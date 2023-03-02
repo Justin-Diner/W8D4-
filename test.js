b885cad013ad0ce033c962844f14c43eb38de361
useNavigate <useNavigate@gmail.com>
2023-03-02 16:32:37 -0500

    we are DONE!

--- test.js
+++ test.js
@@ -1,28 +1,35 @@
 class Clock {
-	constructor() {
-		this.hour = Date().slice(16, 18);
-		this.minute = Date().slice(19, 21);
-		this.second = Date().slice(22, 24);
-		this.printTime();
+  constructor() {
+    this.date = new Date();
+    this.hour = this.date.getHours();
+    this.minute = this.date.getMinutes();
+    this.second = this.date.getSeconds();
+    this.printTime();
 
-	  // 1. Create a Date object.
-	  // 2. Store the hours, minutes, and seconds.
-	  // 3. Call printTime.
-	  // 4. Schedule the tick at 1 second intervals.
-	}
-  
-	printTime() {
-		const hour = parseInt(this.hour) % 12 === 0 ? 12 : parseInt(this.hour) % 12
+    setInterval(this._tick.bind(this), 1000);
 
-		return console.log(`${hour}:${this.minute}:${this.hour}`)
-	  // Format the time in HH:MM:SS
-	  // Use console.log to print it.
-	}
-  
-	_tick() {
-	  // 1. Increment the time by one second.
-	  // 2. Call printTime.
-	}
+    // 1. Create a Date object.
+    // 2. Store the hours, minutes, and seconds.
+    // 3. Call printTime.
+    // 4. Schedule the tick at 1 second intervals.
   }
-  
-  const clock = new Clock();
+
+  printTime() {
+    const hour = parseInt(this.hour) % 12 === 0 ? 12 : parseInt(this.hour) % 12;
+
+    return console.log(`${hour}:${this.minute}:${this.second}`);
+    // Format the time in HH:MM:SS
+    // Use console.log to print it.
+  }
+
+  _tick() {
+	this.date = new Date();
+	this.hour = this.date.getHours();
+    this.minute = this.date.getMinutes();
+    this.second = this.date.getSeconds();
+	this.printTime()
+  }
+}
+
+const clock = new Clock();
+
