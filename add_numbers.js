48f7ec8e9531a3f5897fb7750e15f20fea6dce7f
Justin <jcdiner@gmail.com>
2023-03-02 17:25:20 -0500

    almost theregit add .!

--- add_numbers.js
+++ add_numbers.js
@@ -1,3 +1,4 @@
+const { rawListeners } = require('process');
 const readline = require('readline')
 
 const reader = readline.createInterface({
@@ -8,18 +9,20 @@ const reader = readline.createInterface({
 function addNumbers(sum, numsLeft, callBack) {
 	if (numsLeft > 0) {
 		reader.question('Please Input a Number', function(answer) {
-			callBack(answer)
-			addNumbers(sum,numsLeft-1,callBack)
-		})
+			callBack(sum, answer);
+			addNumbers(sum,numsLeft-1,callBack);
+		});
+		
 	} else if (numsLeft === 0){
-		callBack(sum)
+		callBack(sum);
 	}
 }
 
-function callBack(answer){
-	let numAnswer =  parseInt(answer)
-	sum+= numAnswer
+function callBack(sum, answer){
+	let numAnswer =  parseInt(answer);
+	sum += numAnswer;
 	console.log(sum)
+	return sum;
 }
 
 
